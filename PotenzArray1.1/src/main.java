c9e62d5fffd7c2fcd139e2edfc78f9c1622e8208
Supawat Yokubol <Supawat@Supawat-Yokubols-MacBook-Pro.local>
2015-03-20 20:42:50 +0100

    Hausaufgabe von 20.03.15

---
+++ PotenzArray1.1/src/main.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Supawat
+ * Stk TU Darmstadt T378
+ * 
+ * Last update: Fri 20.03.15
+ *
+ */
+
+public class main {										//eine neue Klasse hei§t "main"
+	public static void main(String[] args){				//diese Methode bezeichnet, dass diese Methode main-Methode ist
+		PotenzArray anArray = new PotenzArray();		//erzeugt neue Instanz hei§t "pa"
+		anArray.berechneZahlen();						//die Instanz "anArray" ruf die Methode "berechneZahlen" auf
+	}
+
+}
